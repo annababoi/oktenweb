6f7ac668f51df6b783704ac10b0b9c4e88ec6382
annababoi <annababoy07@gmail.com>
2022-07-22 17:35:35 +0300

    directory

---
+++ js/Lesson_1/main.js
@@ -0,0 +1,94 @@
+
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let a1 = "hello";
+// let a2 = "owu";
+// let a3 = "com";
+// let a4 = "ua";
+// let a5 = 1;
+// let a6 = 10;
+// let a7 = -999;
+// let a8 = 123;
+// let a9 = 3.14;
+// let a10 = 2.7;
+// let a11 = 16;
+// let a12 = true;
+// let a13 = false;
+//
+// console.log(a1);
+// alert(a1);
+// document.write(a1);
+//
+// console.log(a2);
+// alert(a2);
+// document.write(a2);
+//
+// console.log(a3);
+// alert(a3);
+// document.write(a3);
+//
+// console.log(a4);
+// alert(a4);
+// document.write(a4);
+//
+// console.log(a5);
+// alert(a5);
+// document.write(a5);
+//
+// console.log(a6);
+// alert(a6);
+// document.write(a6);
+//
+// console.log(a7);
+// alert(a7);
+// document.write(a7);
+//
+// console.log(a8);
+// alert(a8);
+// document.write(a8);
+//
+// console.log(a9);
+// alert(a9);
+// document.write(a9);
+//
+// console.log(a10);
+// alert(a10);
+// document.write(a10);
+//
+// console.log(a11);
+// alert(a11);
+// document.write(a11);
+//
+// console.log(a12);
+// alert(a12);
+// document.write(a12);
+//
+// console.log(a13);
+// alert(a13);
+// document.write(a13);
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+// let firstName = "Анна ";
+// let middleName = "Михайлівна ";
+// let lastMame = "Бабой";
+// let person = firstName.concat(middleName, lastMame);
+// console.log(person);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+// let a = 100;
+// console.log(typeof a);
+// let b = '100';
+// console.log(typeof b);
+// let c = true;
+// console.log(typeof c);
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+// let firstName = prompt("Anna");
+// console.log(firstName);
+// let middleName = prompt("Михайлівна");
+// console.log(middleName);
+// let lastName = prompt("Бабой");
+// console.log(lastName);
+
+
