dc18ab874f2b710418dfc2dd6fba8fd0d35b4688
annababoi <annababoy07@gmail.com>
2022-07-27 17:02:59 +0300

    directory

---
+++ js/Lesson_2/script.js
@@ -0,0 +1,128 @@
+// // - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// let colors = ["white", "grey", "brown", "black", "pink", "red", "orange", "yellow", "blue", "green"];
+// console.log(colors.length);
+// console.log(colors[0]);
+// console.log(colors[1]);
+// console.log(colors[2]);
+// console.log(colors[3]);
+// console.log(colors[4]);
+// console.log(colors[5]);
+// console.log(colors[6]);
+// console.log(colors[7]);
+// console.log(colors[8]);
+// console.log(colors[9]);
+//
+
+// // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// let gucciHouse = {
+//     title: "The House of Gucci",
+//     pageCount: 400,
+//     genre: ["biography", "document detective"]
+// };
+// let Freakonomiks = {
+//     title: "Freakonomics: A Rogue Economist Explores the Hidden Side of Everything",
+//     pageCount: 248,
+//     genre: "sociology"
+// };
+// let JSForChildren = {
+//     title: "JavaScript для дітей. Веселий вступ до програмування",
+//     pageCount: 408,
+//     genre: ["development", "programming languages"]
+// };
+// console.log(gucciHouse, Freakonomiks, JSForChildren);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+//
+// let blueMoon = {
+//     title: "George and the Blue Moon",
+//     pageCount: 336,
+//     genre: "Science Fiction",
+//     authors: [
+//         {name: "Lucy Hawking", age: 53},
+//         {name: "Stephen Hawking", age: 76}
+//     ]
+// };
+//
+// let marshmallow = {
+//     title: "Don't Eat The Marshmallow.",
+//     pageCount: 112,
+//     genre: "psychology",
+//     authors: [
+//         {name: "Joachim de Posada", age: 68},
+//         {name: "Ellen Singer", age: 70}
+//     ]
+// };
+//
+// let eureka = {
+//     title: "The Eureka Factor: Aha Moments, Creative Insight, and the Brain",
+//     pageCount: 368,
+//     genre: "science",
+//     authors: [
+//         {name: "John Kounios", age: "near 60"},
+//         {name: "Mark Beeman", age: "near 60"}
+//     ]
+// };
+//
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+let users = [
+    {
+        name: "Bred Pitt",
+        username: "Pitty",
+        password: 37273672632
+    },
+    {
+        name: "Matthew David McConaughey",
+        username: "mat245",
+        password: 23321111
+    },
+    {
+        name: "Johnny Depp",
+        username: "DeppJ",
+        password: 43423233
+    },
+    {
+        name: "Madonna",
+        username: "Mad6969",
+        password: 3234232
+    },
+    {
+        name: "Leonardo di Caprio",
+        username: "Leo222",
+        password: 434343434
+    },
+    {
+        name: "Morgan Freeman",
+        username: "freeMorgy",
+        password: 323232323
+    },
+    {
+        name: "Will Smith",
+        username: "FunnySmith",
+        password: 32323232332
+    },
+    {
+        name: "Adam Sendler",
+        username: "americanPie",
+        password: 20382947922
+    },
+    {
+        name: "Bradly Cooper",
+        username: "384867983",
+        password: 21212121
+    },
+    {
+        name: "Devid Duhovni",
+        username: "DDuh",
+        password: 38929999200
+    },
+];
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
